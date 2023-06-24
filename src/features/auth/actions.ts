b2f61874663c7cda7e148ffabf08dae3e60e11ed
Ivan Partida <ivan.97galarz@gmail.com>
2023-06-23 23:44:19 -0700

    chore: remove unused file

--- src/features/auth/actions.ts
+++
@@ -1,3 +0,0 @@
-export { default as registerUser } from './actions/registerUser.action.js'
-export { default as verifyUser } from './actions/verifyUser.action.js'
-export { default as login } from './actions/login.action.js'
